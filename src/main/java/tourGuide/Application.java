a099e90c0316ebfa37d3a500f2914719559f0fe0
BATIONO Aristide <aristide.laurent@gmail.com>
2020-08-25 12:59:31 +0200

    add swaguer doc

--- src/main/java/tourGuide/Application.java
+++ src/main/java/tourGuide/Application.java
@@ -2,9 +2,10 @@ package tourGuide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/tourGuide/controller/TourGuideController.java
+++ src/main/java/tourGuide/controller/TourGuideController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import gpsUtil.location.Attraction;
 import gpsUtil.location.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
